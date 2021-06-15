6f681ba4b4ea3b8e58a9faef3196527d4cd83178
Vangelis <vpaklatzis@gmail.com>
2021-06-15 21:18:35 +0300

    Created new-image API endpoint and request is now completed by flask

---
+++ api/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import requests 
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+UNSPLASH_URL="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Create .env file inside the API folder and write the Unsplash API access key")
+
+app = Flask(__name__)
+
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    headers = {
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID " + UNSPLASH_KEY
+    }
+    params = {"query": word}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+   
+    data = response.json()
+    return data
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5050)
